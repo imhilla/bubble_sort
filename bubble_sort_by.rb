9352d6950b5388a80efc71d13c1c197edeace71e
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-11 16:44:41 +0300

    Update bubble_sort_by.rb

---
+++ bubble_sort_by.rb
@@ -0,0 +1,20 @@
+class Array
+    def bubble_sort_by
+        n = self.length
+        loop do
+            swapped = false
+            (n-1).times do |i|
+                if yield (self[i] > self[i + 1]) > 0
+                    self[i], self[i + 1] = self[i + 1], self[i]
+                    swapped = true
+                end
+            end
+            break if not swapped
+        end
+        self
+    end
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|  
+    left.length - right.length
+end
