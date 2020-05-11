c4e11884fad80110bf095f650fa5fa262c029486
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-11 15:23:51 +0300

    added array

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -17,3 +17,24 @@ end
 
 bubble_sort([1,5,7,2,3])
 
+#bubble_sort.rb
+class Array
+    def bubble_sort
+        n = self.length
+        loop do
+            swapped = false
+            (n-1).times do |i|
+                if self[i] > self[i+1]
+                    self[i], self[i+1] = self[i+1], self[i]
+                    swapped = true
+                end
+            end
+            break if not swapped
+        end
+        self
+    end
+end
+
+arr = [5, 7, 4, 9, 2, 4, 5]
+arr.bubble_sort
+
