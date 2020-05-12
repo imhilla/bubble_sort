10a03b8ccc456325887df69f31c832d285ebf6b5
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-12 11:24:02 +0300

    Update bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -30,6 +30,6 @@ def bubble_sort_by(array)
   pp array
 end
 
-bubble_sort_by(['hi', 'hello', 'hey]) do |left, right|
+bubble_sort_by(['hi', 'hello', 'hey']) do |left, right|
   left.length - right.length
 end
