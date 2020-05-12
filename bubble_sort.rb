ccd12cc93937651c1ed5770d4f61835e5745a00c
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-12 07:24:38 +0300

    changed indentation

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,48 +1,39 @@
 class Array
-    def bubble_sort
-        n = self.length
-        loop do
-            swapped = false
-            (n-1).times do |i|
-                if self[i] > self[i + 1]
-                    self[i], self[i + 1] = self[i + 1], self[i]
-                    swapped = true
-                end
-            end
-            break if not swapped
+  def bubble_sort
+    n = self.length
+    loop do
+      swapped = false
+      (n-1).times do |i|
+        if self[i] > self[i + 1]
+          self[i], self[i + 1] = self[i + 1], self[i]
+          swapped = true
         end
-        self
+      end
+      break if not swapped
     end
+    self
+  end
 end
 
 arr = [5, 7, 4, 9, 2, 4, 5]
 puts arr.bubble_sort
 
 def bubble_sort_by(array)
-    n = array.length
-    loop do
-        swapped = false
-        (n-1).times do |i|
-            if yield( array[i], array[i + 1]).positive?
-                array[i], array[i + 1] = array[i + 1], array[i]
-                swapped = true
-            end
-        end
-        break unless swapped
+  n = array.length
+  loop do
+    swapped = false
+    (n-1).times do |i|
+      if yield( array[i], array[i + 1]).positive?
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
+      end
     end
-    pp array
+    break unless swapped
+  end
+  pp array
 end
 
 bubble_sort_by(["hi","hello","hey"]) do |left,right|
-    left.length - right.length
+  left.length - right.length
 end
-
-
-
-
-
-
-
-
-
 
