4008e54957a7c0a4ab27569b131d77eea9755fff
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-12 06:44:29 +0300

    defined the bubble_sort by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -15,10 +15,27 @@ class Array
     end
 end
 
-
 arr = [5, 7, 4, 9, 2, 4, 5]
-arr.bubble_sort
+puts arr.bubble_sort
 
+def bubble_sort_by(array)
+    n = array.length
+    loop do
+        swapped = false
+        (n-1).times do |i|
+            if yield( array[i], array[i + 1]).positive?
+                array[i], array[i + 1] = array[i + 1], array[i]
+                swapped = true
+            end
+        end
+        break unless swapped
+    end
+    pp array
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+    left.length - right.length
+end
 
 
 
--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -4,7 +4,7 @@ class Array
         loop do
             swapped = false
             (n-1).times do |i|
-                if yield (self[i] > self[i + 1]) > 0
+                if yield( self[i], self[i + 1]) > 0
                     self[i], self[i + 1] = self[i + 1], self[i]
                     swapped = true
                 end
@@ -15,7 +15,6 @@ class Array
     end
 end
 
-# sorted = bubble_sort_by(["hi","hello","hey"]) do |left,right|  
-#     left.length - right.length
-# end
-# puts sorted
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+    left.length - right.length
+end
