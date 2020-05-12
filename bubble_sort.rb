12115d7a3be7c88d70a77a4354d3ec1661f5acd9
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-12 13:10:04 +0300

    Update bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,7 +3,7 @@ def bubble_sort(array)
     swapped = false
     array.each_with_index do |_val, index|
       next if index >= array.length - 1
-      
+
       if array[index] > array[index + 1]
         array[index], array[index + 1] = array[index + 1], array[index]
         swapped = true
