e7542a96cb5b08804bf126930523185b86e37ebe
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-12 11:11:41 +0300

    Update bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,22 +1,20 @@
-class Array
-  def bubble_sort
-    n = self.length
-    loop do
-      swapped = false
-      (n - 1).times do |i|
-        if self[i] > self[i + 1]
-          self[i], self[i + 1] = self[i + 1], self[i]
-          swapped = true
-        end
+def bubble_sort(array)
+  n = array.size
+  loop do
+    swapped = false
+    (n - 1).times do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
       end
-      break unless swapped
     end
-    self
+    break unless swapped
   end
+  pp array
 end
 
 def bubble_sort_by(array)
-  n = array.length
+  n = array.size
   loop do
     swapped = false
     (n - 1).times do |i|
